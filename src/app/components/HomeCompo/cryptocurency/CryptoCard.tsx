856545c3a315427c50fe49d0a94275fd89576039
Rakib <rakib172890@gmail.com>
2024-10-05 17:38:36 +0600

    text-change

--- src/app/components/HomeCompo/cryptocurency/CryptoCard.tsx
+++ src/app/components/HomeCompo/cryptocurency/CryptoCard.tsx
@@ -14,10 +14,10 @@ interface CryptoCardProps {
 
 const CryptoCard: React.FC<CryptoCardProps> = ({image, name, price, percentage, isIncrease }) => {
   return (
-    <div className="dark:text-white dark:border rounded-2xl shadow px-6 py-2 w-full">
+    <div className="dark:text-white dark:border rounded-2xl shadow px-2 lg:px-6 py-2 w-full">
       <Image width={50} src={image} alt='image' className='py-2' />
       <h3 className="text-base font-semibold ">{name}</h3>
-      <p className="text-gray-700 mb-4">BDT {price}</p>
+      <p className="text-gray-700 text-sm mb-4">BDT {price}</p>
       <p className={`text-3xl  ${isIncrease ? 'text-green-800' : 'text-red-500'} flex items-center justify-center gap-2`}>
         {isIncrease ? <FaArrowUp /> : <FaArrowDown />} {percentage}%
       </p>
